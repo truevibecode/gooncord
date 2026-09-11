cfab025fc6a5633eb5f4c25467aaad3ed40f697d
truevibecode <dev@truevibecode.org>
2026-09-11 10:42:49 +0200

    feat: move watermark to the far left of header bar

--- src/equicordplugins/_api/headerBar.ts
+++ src/equicordplugins/_api/headerBar.ts
@@ -16,8 +16,8 @@ export default definePlugin({
         {
             find: '?"BACK_FORWARD_NAVIGATION":',
             replacement: {
-                match: /(?<="HELP"===.{0,75}\{\}\))(?=\])/,
-                replace: ",...Vencord.Api.HeaderBar._addHeaderBarButtons()"
+                match: /(?<=return\s*\[)/,
+                replace: "...Vencord.Api.HeaderBar._addHeaderBarButtons(),"
             }
         },
         {
